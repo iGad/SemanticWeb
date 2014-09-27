cd9fa4233ca693f9fecbc96e26dbe66c01832793
iGad <pavel.odegov@gmail.com>
2014-09-27 14:11:46 +0400

    Добавил предварительное описание классов
    Добавил предварительное описание классов для сем. сети. Теперь нужно
    почитать про визуализацию и прикрутить ее.

---
+++ MASSemanticWeb/SemanticNode and Link.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+
+
+namespace MASSemanticWeb
+{
+    public enum LinkDirection
+    {
+        Inner,
+        Outter,
+        Both
+    }
+
+    public class SemanticNode
+    {
+        private string _name;
+        private string _comment;
+        public EventHandler OnChange;
+        public event EventHandler Change;
+        private List<SemanticLink> _inLinks;
+        private List<SemanticLink> _outLinks;
+        private Point _position;//местоположение левого верхнего угла
+        private int _width;
+        private int _height;
+
+        #region Свойства
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                if (OnChange != null) Change(this, EventArgs.Empty); //OnChange(this, EventArgs.Empty);
+            }
+        }
+
+        public string Comment
+        {
+            get { return _comment; }
+            set
+            {
+                _comment = value;
+                if(Change != null) Change(this, EventArgs.Empty);
+            }
+        }
+
+        public List<SemanticLink> InLinks
+        {
+            get { return _inLinks; }
+            private set
+            {
+                _inLinks = value;
+            }
+        }
+
+        public List<SemanticLink> OutLinks
+        {
+            get { return _outLinks; }
+            private set
+            {
+                _outLinks = value;
+                
+            }
+        }
+
+        public Point Position
+        {
+            get { return _position; }
+            set { _position = value; }
+        }
+
+        public int Width
+        {
+            get { return _width; }
+            set { _width = value; }
+        }
+
+        public int Height
+        {
+            get { return _height; }
+            set { _height = value; }
+        }
+
+//изменение позиции не считается изменением
+        #endregion
+
+        public SemanticNode(string name, string comment, Point position, int width, int height)
+        {
+            _name = name;
+            _comment = comment;
+            _position = position;
+            _width = width;
+            _height = height;
+            _inLinks = new List<SemanticLink>();
+            _outLinks = new List<SemanticLink>();
+        }
+
+        public void AddLink(LinkDirection direction, SemanticLink link)
+        {
+            if (direction != LinkDirection.Outter)
+                _inLinks.Add(link);
+            if(direction!= LinkDirection.Inner)
+                _outLinks.Add(link);
+            if(Change != null) Change(this, EventArgs.Empty);
+        }
+
+        public void RemoveLink(LinkDirection direction, SemanticLink link)
+        {
+            if (direction != LinkDirection.Inner)
+            {
+                if (_outLinks.Contains(link))
+                    _outLinks.Remove(link);
+            }
+            if (direction != LinkDirection.Outter)
+            {
+                if (_inLinks.Contains(link))
+                    _inLinks.Remove(link);
+            }
+        }
+
+        public void Dispose()
+        {
+            _outLinks = null;
+            _inLinks = null;
+
+        }
+    }
+
+    public class SemanticLink
+    {
+        private string _name;
+        private string _comment;
+        public EventHandler OnChange;
+        public event EventHandler Change;
+        private Color _color;
+        private Bitmap _image;
+
+        public string Name
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                if(Change != null) Change(this, EventArgs.Empty);
+            }
+        }
+
+        public string Comment
+        {
+            get { return _comment; }
+            set
+            {
+                _comment = value;
+                if(Change != null) Change(this, EventArgs.Empty);
+            }
+        }
+
+        public Color Color
+        {
+            get { return _color; }
+            set
+            {
+                _color = value;
+                if(Change != null) Change(this, EventArgs.Empty);
+            }
+        }
+
+        public Bitmap Image
+        {
+            get { return _image; }
+            set
+            {
+                _image = value;
+                if(Change != null) Change(this, EventArgs.Empty);
+            }
+        }
+
+        public SemanticLink(string name, string comment, Color color, Bitmap image)
+        {
+            this.Comment = comment;
+            this._name = name;
+            this.Color = color;
+            this.Image = image;
+        }
+
+        public static SemanticLink CreateNew(string name, string comment, Color color, Bitmap image)
+        {
+            return new SemanticLink(name, comment, color, image);
+        }
+    }
+}
---
+++ MASSemanticWeb/SemanticWeb.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MASSemanticWeb
+{
+
+    public class SemanticWeb
+    {
+        private List<SemanticNode> _nodes;
+        private List<SemanticLink> _links;
+        public EventHandler OnChange;
+        public event EventHandler Change;
+
+        public List<SemanticNode> Nodes
+        {
+            get { return _nodes; }
+            set
+            {
+                _nodes = value;
+                if(Change!=null) Change(this, EventArgs.Empty);
+            }
+        }
+
+        public List<SemanticLink> Links
+        {
+            get { return _links; }
+            set
+            {
+                _links = value;
+                if (Change != null) Change(this, EventArgs.Empty);
+            }
+        }
+
+        public SemanticWeb()
+        {
+            Nodes = new List<SemanticNode>();
+            Links = new List<SemanticLink>();
+            
+        }
+
+        #region Работа с узлами и связями
+
+        public void AddNode(string name, string comment, Point position)
+        {
+            int width = name.Length<15?name.Length:name.Length%15, height=20*(name.Length/15);
+            SemanticNode node = new SemanticNode(name, comment, position, width, height);
+            node.Change += node_Change;
+            Nodes.Add(node);
+            Change(this, EventArgs.Empty);
+        }
+
+        public void AddLink(string name, string comment, Color color, Bitmap image)
+        {
+            SemanticLink link = new SemanticLink(name, comment, color, image);
+            link.Change += link_Change;
+            Links.Add(link);
+            Change(this, EventArgs.Empty);
+        }
+
+        public void RemoveNode(SemanticNode node)
+        {
+            if (!Nodes.Contains(node))
+                return;
+            node.Change -= node_Change;
+            Nodes.Remove(node);
+        }
+
+        public void RemoveNodeAt(int index)
+        {
+            if (index < 0 || index >= Nodes.Count)
+                return;
+            Nodes[index].Change -= node_Change;
+            Nodes.RemoveAt(index);
+        }
+
+        public void RemoveLink(SemanticLink link)
+        {
+            if (!Links.Contains(link))
+                return;
+            link.Change -= link_Change;
+            Links.Remove(link);
+        }
+
+        public void RemoveLinkAt(int index)
+        {
+            if (index < 0 || index >= Links.Count)
+                return;
+            Links[index].Change -= link_Change;
+            Links.RemoveAt(index);
+        }
+
+        public void EditNode(SemanticNode node, string name, string comment)
+        {
+            if (!Nodes.Contains(node))
+                return;
+            if (!node.Name.ToUpper().Equals(name.ToUpper().Trim()))
+                node.Name = name;
+            if (!node.Comment.ToUpper().Equals(comment.ToUpper().Trim()))
+                node.Comment = comment;
+        }
+
+        public void EditLink(SemanticLink link, string name, string comment, Color color, Bitmap image)
+        {
+            if (!Links.Contains(link))
+                return;
+            if (!link.Name.ToUpper().Equals(name.ToUpper().Trim()))
+                link.Name = name;
+            if (!link.Comment.ToUpper().Equals(comment.ToUpper().Trim()))
+                link.Comment = comment;
+            link.Color = color;
+            link.Image = image;
+        }
+
+        public void AddLinkForNode(SemanticNode node, SemanticLink link, LinkDirection direction)
+        {
+            node.AddLink(direction, link);
+        }
+
+        public void RemoveLinkForNode(SemanticNode node, SemanticLink link, LinkDirection direction)
+        {
+            node.RemoveLink(direction, link);
+        }
+
+        #endregion
+
+        void link_Change(object sender, EventArgs e)
+        {
+
+        }
+
+        void node_Change(object sender, EventArgs e)
+        {
+
+        }
+
+        public void Draw(Graphics graphics)
+        {
+            
+        }
+    }
+}
